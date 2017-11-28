507cee2c560d3dc58765678ea081a66ae5c57e0a
sudanamin <sudanamin@hotmail.com>
2017-11-28 23:11:20 +0400

    add resize basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 declare var $: any;
 
+import * as elementResizeDetectorMaker from 'element-resize-detector';
+
+  
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,8 +15,24 @@ export class AppComponent {
   ngOnInit() { 
     
     console.log(`hi from inside commponent `); 
-    $("#amin").draggable();
-  
+  //  $("#amin").draggable();
+
+
+    let _elementResizeDetector = elementResizeDetectorMaker({
+      strategy: 'scroll'
+    });
+
+    _elementResizeDetector.listenTo(document.getElementById('amin'),function(element) {
+      var width = element.offsetWidth;
+      var height = element.offsetHeight;
+      console.log("Size: " + width + "x" + height);
+  })
+
+  _elementResizeDetector.listenTo(document.getElementById('abc'),function(element) {
+    var width = element.offsetWidth;
+    var height = element.offsetHeight;
+    console.log("Size: " + width + "x" + height);
+})
   
   }
 }
