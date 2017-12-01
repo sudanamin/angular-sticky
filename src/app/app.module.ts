5a1b94d2be28888bbdb77030587adfbe86ca0609
sudanamin <sudanamin@hotmail.com>
2017-12-01 11:08:14 +0400

    adding dependencies firesore, angularfire2 and firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,29 @@ import { AppComponent } from './app.component';
 import { StickyComponentComponent } from './sticky-component/sticky-component.component';
 
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBs3l827jDcOmniFPenLdYA9AqS2pNRPxE",
+  authDomain: "angular-sticky.firebaseapp.com",
+  databaseURL: "https://angular-sticky.firebaseio.com",
+  projectId: "angular-sticky",
+  storageBucket: "angular-sticky.appspot.com",
+  messagingSenderId: "655107197844"
+  }
+
+  
 @NgModule({
   declarations: [
     AppComponent,
     StickyComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
